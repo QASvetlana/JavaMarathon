60773d147b95c168fe79f0cc248ffa98e509e7cc
Sveta Andreeva <89111378147v@yandex.com>
2022-11-13 02:43:47 +0300

    Initial commit

---
+++ src/main/java/day1/Task6.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int i = 2;
+        int y;
+        for (int k = 1; k <= 9; k++) {
+            y = k * i;
+            System.out.println(k + " x " + i + " = " + y);
+
+        }
+    }
+}
