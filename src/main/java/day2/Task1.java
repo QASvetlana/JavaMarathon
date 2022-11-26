0fafde920fbcba8f27822cc27426e6060c6b5de2
Sveta Andreeva <89111378147v@yandex.com>
2022-11-26 11:08:34 +0300

    added answer

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,19 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        int quantity = s.nextInt();
+        if (quantity <= 4 && quantity > 0) {
+            System.out.println("Малоэтажный дом");
+        } else if (quantity > 4 && quantity < 9) {
+            System.out.println("Среднеэтажный дом");
+        } else if (quantity >= 9) {
+            System.out.println("Многоэтажный дом");
+        } else {
+            System.out.println("Ошибка ввода");
+        }
     }
 }
