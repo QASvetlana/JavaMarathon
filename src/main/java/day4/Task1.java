e4fd18eae1252c3eba12bd6329a95aa01595fc58
Sveta Andreeva <89111378147v@yandex.com>
2022-12-08 02:25:15 +0300

    added answers for day 4 first task

--- src/main/java/day4/Task1.java
+++ src/main/java/day4/Task1.java
@@ -1,7 +1,65 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        Random rand = new Random();
+        int[] numbers = new int[n];
+
+        for (int x = 0; x < numbers.length; x++)
+            numbers[x] = rand.nextInt(10);
+        System.out.print(Arrays.toString(numbers));
+
+        System.out.println("Длинна массива: " + numbers.length);
+
+
+        int count = 0;
+        for (int x = 0; x < numbers.length; x++) {
+
+            if (numbers[x] > 8)
+                count++;
+        }
+        System.out.println("Количество чисел больше 8: " + count);
+
+
+        int coun = 0;
+        for (int x = 0; x < numbers.length; x++) {
+
+            if (numbers[x] == 1)
+                coun++;
+        }
+        System.out.println("Количество чисел равных 1: " + coun);
+
+        int cou = 0;
+        for (int x = 0; x < numbers.length; x++) {
+
+            if (numbers[x] % 2 == 0)
+                cou++;
+        }
+        System.out.println("Количество четных чисел: " + cou);
+
+        int co = 0;
+        for (int x = 0; x < numbers.length; x++) {
+
+            if (numbers[x] % 2 != 0)
+                co++;
+        }
+        System.out.println("Количество нечетных чисел: " + co);
+
+
+        int sum = 0;
+        for (int x = 0; x < numbers.length; x++)
+            sum += numbers[x];
+        System.out.println("Сумма всех элементов массива:" + sum);
 
     }
 }
+
+
+
--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -1,7 +1,43 @@
 package day4;
 
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+        Random rand = new Random();
+        int[] numbers = new int[100];
+        int vs[] = new int[numbers.length];
+        int j=-1;
+        int ost;
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(10000);
+             // System.out.print(numbers[i] + " ");
 
+        }
+            int min = numbers[0];
+            for (int i : numbers)
+                if (min > i) {
+                    min = i;
+                }
+             // System.out.println();
+            //  System.out.println(min);
+
+        for (int i=0; i<numbers.length; i++) {
+            ost = numbers[i]%10;
+            if (ost==0){
+                j++;
+                vs[j]=numbers[i];
+
+
+       // ost=0;
+
+
+      //  for (int i=0; i<vs.length; i++)
+         //   if (vs[i]>ost) ost=vs[i];
+
+
+        System.out.println(i+" ");
     }
-}
+        }}}
+
+
