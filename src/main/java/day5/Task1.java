ccd2922d4a3f29102521967f249eea8642c895ec
Sveta Andreeva <89111378147v@yandex.com>
2022-12-25 19:44:25 +0300

    add day 5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,10 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car1 = new Car();
+        car1.setColor("белый");
+        car1.setModel("москвич");
+        car1.setYear(2022);
+        System.out.println(car1.getColor()+" "+car1.getModel()+" "+car1.getYear()+" года выпуска");
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,8 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike1 = new Motorbike("Ява", "красный", 1998);
 
+        System.out.println(motorbike1.getModel()+" "+motorbike1.getColor()+" "+motorbike1.getYear()+" года выпуска");
     }
 }
